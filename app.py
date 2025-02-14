05c7b0bfd34298cef4fe147c4b27a4315ab973d4
jkostka <jacek.kostka92@gmail.com>
2025-02-14 09:02:24 +0100

    nazwy zmiennych na angielskie
    nazwy zmiennych na angielskie

--- app.py
+++ app.py
@@ -6,132 +6,132 @@ app = Flask(__name__)
 
 # Inicjalizacja wartości poza funkcją index
 start_level = 0
-ilosc_prob = 0
-rodzaj_przedmiotu = "proszę wybrać"
-rodzaj_zwoju = "proszę wybrać"
-ilosc_symulacji = 100
+number_of_attemps = 0
+item_type = "please_choice"
+scroll_type = "please_choice"
+number_of_simulations = 100
 
-def modyfikuj_szanse(szanse_bazowe, rodzaj_zwoju):
-    modyfikator = 0
-    if rodzaj_zwoju == "rytualny kamień":
-        modyfikator = 10
-    elif rodzaj_zwoju == "pieczęć boga smoków":
-        modyfikator = 15
+def increase_chance(basic_chance, scroll_type):
+    modificator = 0
+    if scroll_type == "ritual_stone":
+        modificator = 10
+    elif scroll_type == "dragon_god_seal":
+        modificator = 15
 
-    if isinstance(szanse_bazowe, list):
-        szanse_zmodyfikowane = [min(100, szansa + modyfikator) for szansa in szanse_bazowe]
-        return [int(szansa) for szansa in szanse_zmodyfikowane]
-    elif isinstance(szanse_bazowe, dict):
-        szanse_zmodyfikowane = {k: min(100, v + modyfikator) for k, v in szanse_bazowe.items()}
-        return {k: int(v) for k, v in szanse_zmodyfikowane.items()}
+    if isinstance(basic_chance, list):
+        increased_chance = [min(100, chance + modificator) for chance in basic_chance]
+        return [int(chance) for chance in increased_chance]
+    elif isinstance(basic_chance, dict):
+        increased_chance = {k: min(100, v + modificator) for k, v in basic_chance.items()}
+        return {k: int(v) for k, v in increased_chance.items()}
     else:
-        return szanse_bazowe
+        return basic_chance
 
-def symuluj_ulepszanie(start_level, ilosc_prob, rodzaj_przedmiotu, rodzaj_zwoju):
-    if rodzaj_przedmiotu == "talizman":
-        szanse_bazowe = [55, 50, 45, 40, 35, 30, 25, 20, 15, 10]
-    elif rodzaj_przedmiotu == "bron":
-        szanse_bazowe = {
+def start_simulate(start_level, number_of_attemps, item_type, scroll_type):
+    if item_type == "talisman":
+        basic_chance = [55, 50, 45, 40, 35, 30, 25, 20, 15, 10]
+    elif item_type == "serpent_equipment":
+        basic_chance = {
             1: 90, 2: 75, 3: 60, 4: 50, 5: 40, 6: 30,
             7: 25, 8: 15, 9: 8, 10: 5, 11: 5, 12: 5,
             13: 5, 14: 3, 15: 3
         }
     else:
         return None, None
-    szanse = modyfikuj_szanse(szanse_bazowe, rodzaj_zwoju)
-    aktualny_level = start_level
-    historia_ulepszen = [start_level]
+    total_chance = increase_chance(basic_chance, scroll_type)
+    actual_level = start_level
+    upgrade_history = [start_level]
 
-    for _ in range(ilosc_prob):
-        if (rodzaj_przedmiotu == "bron" and aktualny_level >= 15) or \
-           (rodzaj_przedmiotu == "talizman" and aktualny_level >= 200):
-            historia_ulepszen.append(aktualny_level)
+    for _ in range(number_of_attemps):
+        if (item_type == "serpent_equipment" and actual_level >= 15) or \
+           (item_type == "talisman" and actual_level >= 200):
+            upgrade_history.append(actual_level)
             continue
-        indeks_szansy = (aktualny_level - 1) % 10 if rodzaj_przedmiotu == "talizman" else aktualny_level
-        if isinstance(szanse, dict):
-            szansa = szanse.get(aktualny_level + 1, 0)
+        chance_index = (actual_level - 1) % 10 if item_type == "talisman" else actual_level
+        if isinstance(total_chance, dict):
+            chance = total_chance.get(actual_level + 1, 0)
         else:
-            szansa = szanse[indeks_szansy]
-        if szansa == 0:
-            historia_ulepszen.append(aktualny_level)
+            chance = total_chance[chance_index]
+        if chance == 0:
+            upgrade_history.append(actual_level)
             continue
 
-        losowa_liczba = random.randint(1, 100)
-        if losowa_liczba <= szansa:
-            aktualny_level += 1
-            historia_ulepszen.append(aktualny_level)
+        random_number = random.randint(1, 100)
+        if random_number <= chance:
+            actual_level += 1
+            upgrade_history.append(actual_level)
         else:
-            historia_ulepszen.append(aktualny_level)
+            upgrade_history.append(actual_level)
 
-    return aktualny_level, historia_ulepszen
+    return actual_level, upgrade_history
 
 @app.route("/", methods=["GET", "POST"])
 def index():
-    global start_level, ilosc_prob, rodzaj_przedmiotu, rodzaj_zwoju, ilosc_symulacji
+    global start_level, number_of_attemps, item_type, scroll_type, number_of_simulations
 
-    wyniki = None
+    results = None
     error_message = None
 
     if request.method == "POST":
         try:
-            rodzaj_przedmiotu = request.form.get("rodzaj_przedmiotu")
+            item_type = request.form.get("item_type")
             start_level = int(request.form.get("start_level"))
-            ilosc_prob = int(request.form.get("ilosc_prob"))
-            rodzaj_zwoju = request.form.get("rodzaj_zwoju")
-            ilosc_symulacji = int(request.form.get("ilosc_symulacji"))
+            number_of_attemps = int(request.form.get("number_of_attemps"))
+            scroll_type = request.form.get("scroll_type")
+            number_of_simulations = int(request.form.get("number_of_simulations"))
 
-            if rodzaj_przedmiotu == "proszę wybrać":
+            if item_type == "please_choice":
                 error_message = "Proszę wybrać rodzaj przedmiotu."
-            elif rodzaj_przedmiotu == "bron" and rodzaj_zwoju == "proszę wybrać":
+            elif item_type == "serpent_equipment" and scroll_type == "please_choice":
                 error_message = "Proszę wybrać rodzaj zwoju dla broni."
-            elif not 1 <= ilosc_symulacji <= 1000:
+            elif not 1 <= number_of_simulations <= 1000:
                 error_message = "Ilość symulacji musi być w zakresie od 1 do 1000."
             elif start_level < 0:
                 error_message = "Startowy poziom nie może być ujemny."
-            elif ilosc_prob <= 0:
+            elif number_of_attemps <= 0:
                 error_message = "Ilość prób musi być większa od 0."
-            elif rodzaj_przedmiotu == "bron" and start_level >= 15:
+            elif item_type == "serpent_equipment" and start_level >= 15:
                 error_message = "Maksymalny poziom ulepszenia wężowego ekwipunku to +15."
-            elif rodzaj_przedmiotu == "talizman" and start_level >= 200:
+            elif item_type == "talisman" and start_level >= 200:
                 error_message = "Maksymalny poziom ulepszenia talizmanu to +200."    
 
             if error_message:
-                return render_template("index.html", wyniki=wyniki, start_level=start_level, ilosc_prob=ilosc_prob, rodzaj_przedmiotu=rodzaj_przedmiotu, rodzaj_zwoju=rodzaj_zwoju, ilosc_symulacji=ilosc_symulacji, error_message=error_message), 400
+                return render_template("index.html", results=results, start_level=start_level, number_of_attemps=number_of_attemps, item_type=item_type, scroll_type=scroll_type, number_of_simulations=number_of_simulations, error_message=error_message), 400
 
-            koncowe_levele = []
-            for _ in range(ilosc_symulacji):
-                koncowy_level, _ = symuluj_ulepszanie(start_level, ilosc_prob, rodzaj_przedmiotu, rodzaj_zwoju)
-                if koncowy_level is None:
+            end_levels = []
+            for _ in range(number_of_simulations):
+                end_level, _ = start_simulate(start_level, number_of_attemps, item_type, scroll_type)
+                if end_level is None:
                     return "Niepoprawny rodzaj przedmiotu", 400
-                koncowe_levele.append(koncowy_level)
+                end_levels.append(end_level)
 
-            sredni = statistics.mean(koncowe_levele)
-            mediana = statistics.median(koncowe_levele)
+            average = statistics.mean(end_levels)
+            mediana = statistics.median(end_levels)
             try:
-                domina = statistics.mode(koncowe_levele)
+                domina = statistics.mode(end_levels)
             except statistics.StatisticsError:
                 domina = "Brak jednoznacznej dominanty"
 
-            liczniki_poziomow = {}
-            for level in koncowe_levele:
-                liczniki_poziomow[level] = liczniki_poziomow.get(level, 0) + 1
+            level_counters = {}
+            for level in end_levels:
+                level_counters[level] = level_counters.get(level, 0) + 1
 
-            wyniki = {
-                "sredni": sredni,
+            results = {
+                "average": average,
                 "mediana": mediana,
                 "domina": domina,
                 "start": start_level,
-                "prob": ilosc_prob,
-                "rodzaj_przedmiotu": rodzaj_przedmiotu,
-                "rodzaj_zwoju": rodzaj_zwoju,
-                "liczniki": liczniki_poziomow,
-                "iloscsymulacji": ilosc_symulacji
+                "attemps": number_of_attemps,
+                "item_type": item_type,
+                "scroll_type": scroll_type,
+                "counters": level_counters,
+                "number_of_simulations": number_of_simulations
             }
         except ValueError:
             error_message = "Wprowadź poprawne dane liczbowe."
-            return render_template("index.html", wyniki=wyniki, start_level=start_level, ilosc_prob=ilosc_prob, rodzaj_przedmiotu=rodzaj_przedmiotu, rodzaj_zwoju=rodzaj_zwoju, ilosc_symulacji=ilosc_symulacji, error_message=error_message), 400
+            return render_template("index.html", results=results, start_level=start_level, number_of_attemps=number_of_attemps, item_type=item_type, scroll_type=scroll_type, number_of_simulations=number_of_simulations, error_message=error_message), 400
 
-    return render_template("index.html", wyniki=wyniki, start_level=start_level, ilosc_prob=ilosc_prob, rodzaj_przedmiotu=rodzaj_przedmiotu, rodzaj_zwoju=rodzaj_zwoju, ilosc_symulacji=ilosc_symulacji, error_message=error_message)
+    return render_template("index.html", results=results, start_level=start_level, number_of_attemps=number_of_attemps, item_type=item_type, scroll_type=scroll_type, number_of_simulations=number_of_simulations, error_message=error_message)
 
 if __name__ == "__main__":
     app.run(debug=True)
